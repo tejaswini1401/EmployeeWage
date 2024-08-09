ef292da54e3077c47943611dad51f89cfb1fe5cf
tejaswini_14 <shelaketejaswini14@gmail.com>
2024-08-09 17:40:04 +0530

    Added: EmployeeAttendance

--- EmployeeWageProblem/src/com/solution/EmployeeWage.java
+++ EmployeeWageProblem/src/com/solution/EmployeeWage.java
@@ -1,10 +1,21 @@
 package com.solution;
+import java.util.Random;
 
 public class EmployeeWage {
 
 	public static void main(String[] args) {
 		
 		System.out.println("Employee Wage Problem");
+		
+		Random random = new Random();
+		
+		boolean isPresent = random.nextBoolean();
+		
+		if(isPresent) {
+			System.out.println("Employee is Present");	
+		}else {
+			System.out.println("Employee is not Present");
+		}
 
 	}
 
